2b9b6c23bb81b7cbb129f8ce2b210197b4f3c0b4
gustavomatiass <gustavomatiass123@gmail.com>
2023-01-09 11:19:28 -0300

    primeiro codigo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("digite o seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"olá, {name}");
+Console.Write("digite a data do seu nacimento: ");
+int ano = int.Parse(Console.ReadLine());
+int age = 2023 - ano;
+Console.WriteLine($"vc tem {age} de idade");
